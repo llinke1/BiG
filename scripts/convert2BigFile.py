cc8c13d20d49db222e6bd945e7853edc972b1901
Laila <lailalinke95@gmail.com>
2023-10-02 13:04:30 +0200

    Added script for converting hdf files to bigfile

---
+++ scripts/convert2BigFile.py
@@ -0,0 +1,28 @@
+from nbodykit.source.catalog.file import HDFCatalog
+import h5py
+import argparse
+
+# COMMANDLINE PARSING
+parser = argparse.ArgumentParser(description='Measures 3D bispectrum.')
+
+parser.add_argument("--i", help="Input filename [hdf file]")
+parser.add_argument("--o", help="Output filename [bigfile]")
+
+parser.add_argument("--L", help='box side length [Mpc/h]', type=float)
+parser.add_argument("--Nmesh", help='Number of grid cells along one dimension', type=int)
+
+parser.add_argument("--field", help="Name of column in hdf with right particles, e.g. GrO")
+
+args = parser.parse_args()
+
+if not all(vars(args).values()):
+    parser.error("Not the right number of command line parameters! All are required!")
+
+
+
+
+
+f=HDFCatalog(args.i)
+mesh=f.to_mesh(Nmesh=args.Nmesh, BoxSize=args.L, position=args.field)
+
+mesh.save(args.o, dataset='Field', mode='real')
