906ddd4fe8b3eba4320d1145d7c10eb304126040
Laila <lailalinke95@gmail.com>
2023-10-20 11:51:52 +0200

    Added file for converting HDFCatalog to numpy array

---
+++ scripts/convertHDF2Numpy.py
@@ -0,0 +1,33 @@
+# Script for converting HDF Catalog to numpy array
+# This script requires nbodykit!
+
+from nbodykit.source.catalog.file import HDFCatalog
+import argparse
+import numpy as np
+
+# COMMANDLINE PARSING
+parser = argparse.ArgumentParser(description='Measures 3D bispectrum.')
+
+parser.add_argument("--i", help="Input filename [hdf file]")
+parser.add_argument("--o", help="Output filename [numpy binary]")
+
+parser.add_argument("--L", help='box side length [Mpc/h]', type=float)
+parser.add_argument("--Nmesh", help='Number of grid cells along one dimension', type=int)
+
+parser.add_argument("--field", help="Name of column in hdf with right particles, e.g. GrO")
+
+args = parser.parse_args()
+
+if not all(vars(args).values()):
+    parser.error("Not the right number of command line parameters! All are required!")
+
+
+
+
+
+f=HDFCatalog(args.i)
+mesh=f.to_mesh(Nmesh=args.Nmesh, BoxSize=args.L, position=args.field)
+
+a=mesh.preview()
+
+np.save(args.o, a)
