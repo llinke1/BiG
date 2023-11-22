e8e452949e8c21459608ab8efeedf928e295fe62
Laila <lailalinke95@gmail.com>
2023-11-22 13:28:03 +0100

    Fixed Plotting

--- BiG/plotter.py
+++ BiG/plotter.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+from matplotlib.patches import Polygon, Rectangle
+from matplotlib.colorbar import ColorbarBase
 
 from helpers_plot import initPlot, finalizePlot
 
 class plotter:
-    def __init__ (self, **kwargs) -> None:
+    def __init__ (self, cmap='plasma', **kwargs) -> None:
         initPlot(**kwargs)
 
+        self.cmap=cmap
         self.ks={}
         self.bispecs={}
 
@@ -42,44 +44,42 @@ class plotter:
         self.ratio_edges_high[name]=ratio_edges_high
 
     
-    def plotBispectrum3D(self, outputFn="", showplot=True, tightlayout=False, names=[], vmin=0, vmax=1):
+    def plotBispectrum3D(self, outputFn="", showplot=True, tightlayout=False, names=[], vmin=3, vmax=20):
         if len(names)==0:
             names=list(self.ks.keys())
 
         N=len(names)
 
-
         if N>1:
-            fig, axes=plt.subplots(ncols=N, subplot_kw={'projection': '3d'}, figsize=(N*5+2, 5))
+            fig, axes=plt.subplots(ncols=N, subplot_kw={'projection': '3d'}, figsize=(N*5+5, 6))
 
             for i, name in enumerate(names):
-                #axes[i]=fig.add_subplot(projection='3d')
                 axes[i].set_title(name)
                 img=axes[i].scatter(self.ks[name][:,0], 
                             self.ks[name][:,1], 
                             self.ks[name][:,2], 
-                            self.bispecs[name], vmin=vmin, vmax=vmax)
+                            c=np.log(self.bispecs[name]), cmap=self.cmap, vmin=vmin, vmax=vmax)
                 axes[i].set_xlabel(r'$k_1$ [$h$/Mpc]')
                 axes[i].set_ylabel(r'$k_2$ [$h$/Mpc]')
                 axes[i].set_zlabel(r'$k_3$ [$h$/Mpc]')
-            fig.colorbar(img, ax=axes.ravel(), label=r'$B(k_1, k_2, k_3)$', orientation='vertical')
+            fig.colorbar(img, ax=axes.ravel(), label=r'$\ln[B(k_1, k_2, k_3)]$', orientation='vertical')
         else:
             name=names[0]
-            fig, axes=plt.subplots(subplot_kw={'projection': '3d'}, figsize=(7,5))
+            fig, axes=plt.subplots(subplot_kw={'projection': '3d'}, figsize=(7,6))
             axes.set_title(name)
             img=axes.scatter(self.ks[name][:,0], 
                             self.ks[name][:,1], 
                             self.ks[name][:,2], 
-                            self.bispecs[name], vmin=vmin, vmax=vmax)
+                            c=np.log(self.bispecs[name]), cmap=self.cmap, vmin=vmin, vmax=vmax)
             axes.set_xlabel(r'$k_1$ [$h$/Mpc]')
             axes.set_ylabel(r'$k_2$ [$h$/Mpc]')
             axes.set_zlabel(r'$k_3$ [$h$/Mpc]')
-            fig.colorbar(img, ax=axes, label=r'$B(k_1, k_2, k_3)$', orientation='vertical')
+            fig.colorbar(img, ax=axes, label=r'$\ln[B(k_1, k_2, k_3)]$', orientation='vertical')
         
         
         finalizePlot(axes, outputFn=outputFn, showplot=showplot, tightlayout=tightlayout, showlegend=False)
 
-    def plotBispectrum1D(self, k1_k3=0, k2_k3=0, mode="", outputFn="", showplot=True, tightlayout=True, names=[]):
+    def plotBispectrum1D(self, k1_k3=0, k2_k3=0, mode="", outputFn="", showplot=True, tightlayout=False, names=[], colors=[], markers=[]):
         if len(names)==0:
             names=list(self.ks.keys())
 
@@ -95,12 +95,18 @@ class plotter:
 
         N=len(names)
 
+        if len(colors)==0:
+            cmap=plt.get_cmap(self.cmap)
+            for i in range(N):
+                a=(i+1)/(N+1)
+                c=cmap(a)
+                colors.append(c)
 
 
         fig,ax=plt.subplots()
         ax.loglog()
 
-        for name in names:
+        for i,name in enumerate(names):
             mask=(self.ratio_edges_low[name][:,0]<k1_k3) \
             &(self.ratio_edges_high[name][:,0]>k1_k3) \
             & (self.ratio_edges_low[name][:,1]<k2_k3) \
@@ -108,12 +114,17 @@ class plotter:
 
             k=self.ks[name][:,2][mask]
             bispec=self.bispecs[name][mask]
-   
-            ax.plot(k, bispec, label=name)
+            if len(markers)==0:
+                ax.plot(k, bispec, label=name, c=colors[i])
+            else:
+                ax.plot(k, bispec, label=name, ls=':', marker=markers[i], color=colors[i])
         
-        y=0.5*(k1_k3**2-k2_k3**2+1)
+        #y=0.5*(k2_k3**2-k1_k3**2+1)
+        y=np.sqrt(k2_k3**2-0.25*(k2_k3**2+1-k1_k3**2)**2)
         x=np.sqrt(k1_k3**2-y**2)
         triangle=Polygon([[0,0], [1,0], [x,y]], fc='none', ec='k')
+        line=Polygon([[0,0], [1,0]], fc='none', ec='r', lw=3)
+
 
         ax.set_xlabel(r'$k_3$ [$h$/Mpc]')
         ax.set_ylabel(r'$B($'+f"{k1_k3}"+"$\,k_3,$"+f"{k2_k3}"+r"$\,k_3, k_3)$")
@@ -122,6 +133,8 @@ class plotter:
         ax2=fig.add_axes([0.2,0.2,0.2,0.2])
         ax2.set_axis_off()
         ax2.add_patch(triangle)
+        ax2.add_patch(line)
+
 
         finalizePlot(fig.get_axes, outputFn=outputFn, showplot=showplot, tightlayout=tightlayout, showlegend=False)
         
@@ -140,11 +153,11 @@ class plotter:
                 mask=(self.kbinedges_low[name][:,2]<k3) \
                 &(self.kbinedges_high[name][:,2]>k3)
 
-                img=axes[i].scatter(self.ratio_13[name][mask], self.ratio_23[name][mask], c=self.bispecs[name][mask], vmin=vmin, vmax=vmax)
+                img=axes[i].scatter(self.ratio_13[name][mask], self.ratio_23[name][mask], c=np.log(self.bispecs[name][mask]), cmap=self.cmap, vmin=vmin, vmax=vmax)
 
                 axes[i].set_xlabel(r'$k_1/k_3$')
                 axes[i].set_ylabel(r'$k_2/k_3$')
-            fig.colorbar(img, ax=axes.ravel(), label=r'$B(k_1, k_2, k_3)$', orientation='vertical') 
+            fig.colorbar(img, ax=axes.ravel(), label=r'$\ln[B(k_1, k_2, k_3)]$', orientation='vertical') 
         else:
             name=names[0]
             fig, axes=plt.subplots(figsize=(7,5))
@@ -152,12 +165,76 @@ class plotter:
             mask=(self.kbinedges_low[name][:,2]<k3) \
                 &(self.kbinedges_high[name][:,2]>k3)
 
-            img=axes.scatter(self.ratio_13[name][mask], self.ratio_23[name][mask], c=self.bispecs[name][mask], vmin=vmin, vmax=vmax)
+            img=axes.scatter(self.ratio_13[name][mask], self.ratio_23[name][mask], c=np.log(self.bispecs[name][mask]),cmap=self.cmap, vmin=vmin, vmax=vmax)
 
 
             axes.set_xlabel(r'$k_1/k_3$')
             axes.set_ylabel(r'$k_2/k_3$')
-            fig.colorbar(img, ax=axes, label=r'$B(k_1, k_2, k_3)$', orientation='vertical')
+            fig.colorbar(img, ax=axes, label=r'$\ln[B(k_1, k_2, k_3)]$', orientation='vertical')
+        
+
+        finalizePlot(axes, outputFn=outputFn, showplot=showplot, tightlayout=tightlayout, showlegend=False)
+
+
+    def plotBispectrumTriangleBinsize(self, k3, outputFn="", showplot=True, tightlayout=False, names=[], vmin=0, vmax=1):
+        if len(names)==0:
+            names=list(self.ks.keys())
+        N=len(names)
+
+        if N>1:
+            fig, axes=plt.subplots(ncols=N, figsize=(N*5+2, 5))
+            for i, name in enumerate(names):
+
+                axes[i].set_title(name+r", $k_3=$"+f"{k3}"+ r" $h$/Mpc")
+
+                mask=(self.kbinedges_low[name][:,2]<k3) \
+                &(self.kbinedges_high[name][:,2]>k3)
+
+                r13=self.ratio_13[name][mask]
+                r23=self.ratio_23[name][mask]
+                bispec=np.log(self.bispecs[name][mask])
+                r13_lower=self.ratio_edges_low[name][mask][:,0]
+                r13_upper=self.ratio_edges_high[name][mask][:,0]
+                r23_lower=self.ratio_edges_low[name][mask][:,1]
+                r23_upper=self.ratio_edges_high[name][mask][:,1]
+
+                cmap=plt.get_cmap('plasma')
+                norm=plt.Normalize(vmin, vmax)
+                c=[cmap(norm(d)) for d in bispec]
+
+                axes[i].bar(x=0.5*(r13_upper+r13_lower), height=r23_upper-r23_lower, bottom=r23_lower, width=r13_upper-r13_lower, color=c)
+                axes[i].scatter(r13.ravel(), r23.ravel(), color='k', marker='x', zorder=10)
+
+                axes[i].set_xlabel(r'$k_1/k_3$')
+                axes[i].set_ylabel(r'$k_2/k_3$')
+            cbar = ColorbarBase(ax=fig.add_axes([0.9, 0.1, 0.02, 0.8]), cmap=cmap, norm=norm)
+            cbar.set_label(r'$\ln [B(k_1, k_2, k_3)]$')
+        else:
+            name=names[0]
+            fig, axes=plt.subplots(figsize=(7,5))
+            axes.set_title(name+r", $k_3=$"+f"{k3}"+ r" $h$/Mpc")
+            mask=(self.kbinedges_low[name][:,2]<k3) \
+                &(self.kbinedges_high[name][:,2]>k3)
+
+            r13=self.ratio_13[name][mask]
+            r23=self.ratio_23[name][mask]
+            bispec=np.log(self.bispecs[name][mask])
+            r13_lower=self.ratio_edges_low[name][mask][:,0]
+            r13_upper=self.ratio_edges_high[name][mask][:,0]
+            r23_lower=self.ratio_edges_low[name][mask][:,1]
+            r23_upper=self.ratio_edges_high[name][mask][:,1]
+
+            cmap=plt.get_cmap('plasma')
+            norm=plt.Normalize(vmin, vmax)
+            c=[cmap(norm(d)) for d in bispec]
+
+            axes.bar(x=0.5*(r13_upper+r13_lower), height=r23_upper-r23_lower, bottom=r23_lower, width=r13_upper-r13_lower, color=c)
+            axes.scatter(r13.ravel(), r23.ravel(), color='k', marker='x', zorder=10)
+
+            axes.set_xlabel(r'$k_1/k_3$')
+            axes.set_ylabel(r'$k_2/k_3$')
+            cbar = ColorbarBase(ax=fig.add_axes([0.9, 0.1, 0.02, 0.8]), cmap=cmap, norm=norm)
+            cbar.set_label(r'$\ln [B(k_1, k_2, k_3)]$')
         
 
         finalizePlot(axes, outputFn=outputFn, showplot=showplot, tightlayout=tightlayout, showlegend=False)
