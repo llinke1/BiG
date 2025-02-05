863a969912ac58cc40b387c9c37878b55c725cb7
Laila <lailalinke95@gmail.com>
2025-02-05 10:34:06 +0100

    Adapted version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BiG',
-    version='0.1.3',
+    version='2.0.0_dev',
     description=' Code for measuring 3-D bispectra from density maps using GPUs',
     url='',
     author='Laila Linke',
@@ -13,7 +13,7 @@ setup(
                       'pathlib',
                       'jax'],
     classifiers=[
-        'Development Status :: 2 - PreAlpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: GPU :: NVIDIA CUDA :: 12 :: 12.1'
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  
